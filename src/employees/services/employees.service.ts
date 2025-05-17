579a776eb16cf2a9e171451d059bd7d70d9c1621
nachobek <ignacio.becerra@outlook.com>
2025-05-17 00:43:03 -0300

    feat: add foreign key validation

--- src/employees/services/employees.service.ts
+++ src/employees/services/employees.service.ts
@@ -1,6 +1,9 @@
 import { Injectable, InternalServerErrorException, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
+import { Op } from 'sequelize';
 import ResponseMessages from 'src/common/enums/response-messages.enum';
+import { Department } from 'src/departments/models/department.model';
+import { Job } from 'src/jobs/models/job.model';
 import { CreateEmployee } from '../interfaces/create-employee.interface';
 import { Employee } from '../models/employee.model';
 
@@ -12,6 +15,28 @@ export class EmployeesService {
   ) {}
 
   async bulkCreate(data: CreateEmployee[]) {
+    const uniqueDepartmentIds = new Set(
+      data.map((employee) => employee.departmentId).filter((departmentId) => departmentId !== null),
+    );
+    const uniqueJobIds = new Set(
+      data.map((employee) => employee.jobId).filter((jobId) => jobId !== null),
+    );
+
+    const [departments, jobs] = await Promise.all([
+      Department.findAll({ where: { id: { [Op.in]: Array.from(uniqueDepartmentIds) } } } ),
+      Job.findAll({ where: { id: { [Op.in]: Array.from(uniqueJobIds) } } } ),
+    ]);
+
+    if (departments.length !== uniqueDepartmentIds.size) {
+      Logger.error('Invalid departmentId foreing key', this.constructor.name)
+      throw new InternalServerErrorException(ResponseMessages.EMPLOYEE_CREATION_ERROR);
+    }
+
+    if (jobs.length !== uniqueJobIds.size) {
+      Logger.error('Invalid jobId foreing key', this.constructor.name)
+      throw new InternalServerErrorException(ResponseMessages.EMPLOYEE_CREATION_ERROR);
+    }
+
     const transaction = await this.employeeModel.sequelize.transaction();
 
     try {
